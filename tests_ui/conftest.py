9d8830ce8790b34bf80ea1d9bc63f3211a05a6bb
RemyMiller23 <remymiller23@gmail.com>
2025-05-21 19:18:56 +0200

    edit yaml file

--- tests_ui/conftest.py
+++ tests_ui/conftest.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Playwright
 
 @pytest.fixture()
 def setup(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    browser = playwright.chromium.launch(headless=True, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.facebook.com")
