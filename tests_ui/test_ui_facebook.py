c7e927c739e6ca1937dbfc59fc397490157607c1
RemyMiller23 <remymiller23@gmail.com>
2025-05-21 21:25:58 +0200

    Add secret to YAML

--- tests_ui/test_ui_facebook.py
+++ tests_ui/test_ui_facebook.py
@@ -6,6 +6,9 @@ from pom.home_page_elements import HomePage
 from pom.sign_up_page_elements import SignUpPage
 import pytest
 
+
+PASSWORD = os.environ['PASSWORD']
+
 #@pytest.mark.xfail(reason="not ready")
 @pytest.mark.smoke
 def test_singup(setup) -> None:
@@ -15,7 +18,7 @@ def test_singup(setup) -> None:
     signUp = SignUpPage(page)
 
     home.singUpButton.click()
-    signUp.submit_form("Jess", "Reid", "22", "Feb", "1995", "JessReid@myspy.com", os.environ['PASSWORD'])
+    signUp.submit_form("Jess", "Reid", "22", "Feb", "1995", "JessReid@myspy.com", PASSWORD)
 
     #Success_Message = page.get_by_text("It looks like you may have")
     #expect(Success_Message).to_contain_text("Successfully Created profile")
--- tests_ui/test_ui_facebook_success.py
+++ tests_ui/test_ui_facebook_success.py
@@ -7,11 +7,14 @@ from pom.home_page_elements import HomePage
 from pom.sign_up_page_elements import SignUpPage
 import pytest
 
+
+PASSWORD = os.environ['PASSWORD']
+
 @pytest.mark.smoke
 @pytest.mark.parametrize("name, surname, day, month, year, email, password",
-                         [("Julia", "Gleeson", "30", "Apr", "1999", "JuliaG@myspy.com", os.environ['PASSWORD']),
-                          pytest.param("Megan", "Joshnton", "22", "Mar", "2001", "MegG@myspy.com", os.environ['PASSWORD'], marks=pytest.mark.xfail),
-                          ("Zima", "Miller", "24", "Nov", "1997", "Zima@myspy.com", os.environ['PASSWORD'])])
+                         [("Julia", "Gleeson", "30", "Apr", "1999", "JuliaG@myspy.com", PASSWORD),
+                          pytest.param("Megan", "Joshnton", "22", "Mar", "2001", "MegG@myspy.com", PASSWORD, marks=pytest.mark.xfail),
+                          ("Zima", "Miller", "24", "Nov", "1997", "Zima@myspy.com", PASSWORD)])
 def test_singup_success(setup, name,surname,day,month,year,email,password) -> None:
     page = setup
 
@@ -34,7 +37,7 @@ def test_singup_success_2(setup) -> None:
     signUp = SignUpPage(page)
 
     home.singUpButton.click()
-    signUp.submit_form("Annie", "Baline", "30", "Apr", "2009", "Annie@myspy.com", os.environ['PASSWORD'])
+    signUp.submit_form("Annie", "Baline", "30", "Apr", "2009", "Annie@myspy.com", PASSWORD)
 
     print(page.get_by_text("It looks like you may have"))
 
